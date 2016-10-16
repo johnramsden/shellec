cdbcd10bb7dd13dea6d41b7eeb71c37f39b9e253
John Ramsden <johnramsden@users.noreply.github.com>
2016-10-16 11:37:55 -0700

    Ran successful test script

---
+++ runonall.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#
+
+./shlecffmpeg "Lecture 01" "MATH220-101" \
+"/mnt/Lilan/media/Downloading/Downloads/Complete/School/fflecture/stats-background.jpg" \ "/mnt/Lilan/media/Downloading/Downloads/Complete/School/MATH220/MATH220-E01" \
+"/mnt/Lilan/media/Series/Lectures/MATH 220 Mathematical Proof/2016-2017 Winter Term 01"
+
+./shlecffmpeg "Lecture 02" "MATH220-101" \
+"/mnt/Lilan/media/Downloading/Downloads/Complete/School/fflecture/stats-background.jpg" \ "/mnt/Lilan/media/Downloading/Downloads/Complete/School/MATH220/MATH220-E02" \
+"/mnt/Lilan/media/Series/Lectures/MATH 220 Mathematical Proof/2016-2017 Winter Term 01"
+
+./shlecffmpeg "Lecture 03" "MATH220-101" \
+"/mnt/Lilan/media/Downloading/Downloads/Complete/School/fflecture/stats-background.jpg" \ "/mnt/Lilan/media/Downloading/Downloads/Complete/School/MATH220/MATH220-E03" \
+"/mnt/Lilan/media/Series/Lectures/MATH 220 Mathematical Proof/2016-2017 Winter Term 01"
+
+./shlecffmpeg "Lecture 04" "MATH220-101" \
+"/mnt/Lilan/media/Downloading/Downloads/Complete/School/fflecture/stats-background.jpg" \ "/mnt/Lilan/media/Downloading/Downloads/Complete/School/MATH220/MATH220-E04" \
+"/mnt/Lilan/media/Series/Lectures/MATH 220 Mathematical Proof/2016-2017 Winter Term 01"
+
+./shlecffmpeg "Lecture 05" "MATH220-101" \
+"/mnt/Lilan/media/Downloading/Downloads/Complete/School/fflecture/stats-background.jpg" \ "/mnt/Lilan/media/Downloading/Downloads/Complete/School/MATH220/MATH220-E05" \
+"/mnt/Lilan/media/Series/Lectures/MATH 220 Mathematical Proof/2016-2017 Winter Term 01"
+
+./shlecffmpeg "Lecture 06" "MATH220-101" \
+"/mnt/Lilan/media/Downloading/Downloads/Complete/School/fflecture/stats-background.jpg" \ "/mnt/Lilan/media/Downloading/Downloads/Complete/School/MATH220/MATH220-E06" \
+"/mnt/Lilan/media/Series/Lectures/MATH 220 Mathematical Proof/2016-2017 Winter Term 01"
+
+./shlecffmpeg "Lecture 07" "MATH220-101" \
+"/mnt/Lilan/media/Downloading/Downloads/Complete/School/fflecture/stats-background.jpg" \ "/mnt/Lilan/media/Downloading/Downloads/Complete/School/MATH220/MATH220-E07" \
+"/mnt/Lilan/media/Series/Lectures/MATH 220 Mathematical Proof/2016-2017 Winter Term 01"
+
+./shlecffmpeg "Lecture 08" "MATH220-101" \
+"/mnt/Lilan/media/Downloading/Downloads/Complete/School/fflecture/stats-background.jpg" \ "/mnt/Lilan/media/Downloading/Downloads/Complete/School/MATH220/MATH220-E08" \
+"/mnt/Lilan/media/Series/Lectures/MATH 220 Mathematical Proof/2016-2017 Winter Term 01"
+
+./shlecffmpeg "Lecture 09" "MATH220-101" \
+"/mnt/Lilan/media/Downloading/Downloads/Complete/School/fflecture/stats-background.jpg" \ "/mnt/Lilan/media/Downloading/Downloads/Complete/School/MATH220/MATH220-E09" \
+"/mnt/Lilan/media/Series/Lectures/MATH 220 Mathematical Proof/2016-2017 Winter Term 01"
+
+./shlecffmpeg "Lecture 10" "MATH220-101" \
+"/mnt/Lilan/media/Downloading/Downloads/Complete/School/fflecture/stats-background.jpg" \ "/mnt/Lilan/media/Downloading/Downloads/Complete/School/MATH220/MATH220-E10" \
+"/mnt/Lilan/media/Series/Lectures/MATH 220 Mathematical Proof/2016-2017 Winter Term 01"
+
+./shlecffmpeg "Lecture 11" "MATH220-101" \
+"/mnt/Lilan/media/Downloading/Downloads/Complete/School/fflecture/stats-background.jpg" \ "/mnt/Lilan/media/Downloading/Downloads/Complete/School/MATH220/MATH220-E11" \
+"/mnt/Lilan/media/Series/Lectures/MATH 220 Mathematical Proof/2016-2017 Winter Term 01"
+
+./shlecffmpeg "Lecture 12" "MATH220-101" \
+"/mnt/Lilan/media/Downloading/Downloads/Complete/School/fflecture/stats-background.jpg" \ "/mnt/Lilan/media/Downloading/Downloads/Complete/School/MATH220/MATH220-E12" \
+"/mnt/Lilan/media/Series/Lectures/MATH 220 Mathematical Proof/2016-2017 Winter Term 01"
+
+./shlecffmpeg "Lecture 13" "MATH220-101" \
+"/mnt/Lilan/media/Downloading/Downloads/Complete/School/fflecture/stats-background.jpg" \ "/mnt/Lilan/media/Downloading/Downloads/Complete/School/MATH220/MATH220-E13" \
+"/mnt/Lilan/media/Series/Lectures/MATH 220 Mathematical Proof/2016-2017 Winter Term 01"
+
+./shlecffmpeg "Lecture 14" "MATH220-101" \
+"/mnt/Lilan/media/Downloading/Downloads/Complete/School/fflecture/stats-background.jpg" \ "/mnt/Lilan/media/Downloading/Downloads/Complete/School/MATH220/MATH220-E14" \
+"/mnt/Lilan/media/Series/Lectures/MATH 220 Mathematical Proof/2016-2017 Winter Term 01"
