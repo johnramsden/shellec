04abcf62257ff51448f3ddb8baf3ddfe36ed83b7
John Ramsden <johnramsden@users.noreply.github.com>
2017-01-19 18:17:30 -0800

    Renamed script, added 'cpsc261' script which calls shellec with defaults set.

---
+++ cpsc261.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# On commandline give number and an optional name
+
+# Fill in the following
+lecture_bg="Assets/Intro_bg.jpg"
+lecture_basedir="/mnt/Lilan/Data/Videos/CPSC261-201"
+course="CPSC261-201"
+season="02"
+
+# CLI Input
+lecture_num="${1}"
+name=" ${2}.mp4"
+
+lecture_name="CPSC261 S${season}E${lecture_num}${name:-.mp4}"
+
+~/.local/bin/shellec "Lecture ${lecture_num}" \
+		     ${course} \
+		     ${lecture_basedir}/${lecture_bg} \
+		     ${lecture_basedir}/E${lecture_num} \
+		     ${lecture_basedir}/${lecture_name}
