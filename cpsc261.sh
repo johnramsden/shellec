d5db638577cd2f9e9977d58e41afb6df45e43c80
John Ramsden <johnramsden@users.noreply.github.com>
2017-07-15 13:58:06 -0700

    added author

--- cpsc261.sh
+++ cpsc261.sh
@@ -4,18 +4,20 @@
 
 # Fill in the following
 lecture_bg="Assets/Intro_bg.jpg"
-lecture_basedir="/mnt/Lilan/Data/Videos/CPSC261-201"
+lecture_basedir="/mnt/lilan/Data/Videos/CPSC261-201"
 course="CPSC261-201"
 season="02"
 
 # CLI Input
 lecture_num="${1}"
-name=" ${2}.mp4"
+name="${2:-.mp4}"
 
-lecture_name="CPSC261 S${season}E${lecture_num}${name:-.mp4}"
+lecture_name="CPSC261 S${season}E${lecture_num}${name}"
 
 ~/.local/bin/shellec "Lecture ${lecture_num}" \
 		     ${course} \
 		     ${lecture_basedir}/${lecture_bg} \
 		     ${lecture_basedir}/E${lecture_num} \
 		     ${lecture_basedir}/${lecture_name}
+
+echo "Created ${lecture_name}"
